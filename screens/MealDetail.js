47ad373ef1a18192ba61cdee6156301c629b806c
Luis Rosario <luisalmonter@gmail.com>
2021-01-19 18:28:33 -0500

    adds favoite button in header

--- screens/MealDetail.js
+++ screens/MealDetail.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import {MEALS} from '../data/fake-data';
+
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetail = props => {
     const mealId = props.navigation.getParam('mealId');
@@ -23,7 +26,9 @@ MealDetail.navigationOptions = (navigationData) => {
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
         headerTitle: selectedMeal.title,
-        headerRight: <Text>Fav</Text>
+        headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item  title='Favovite' iconName='ios-star' onPress={() => {console.log('pressed')}} />
+        </HeaderButtons>
     };
 };
 
