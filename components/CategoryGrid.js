aed707af7cba22eed033a58bf2b458773c888a98
Luis Rosario <luisalmonter@gmail.com>
2021-01-18 22:24:03 -0500

    adds style to meal catagory page

--- components/CategoryGrid.js
+++ components/CategoryGrid.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, Platform, TouchableNativeFeedback } from 'react-native';
 
 const CategoryGrid = props => {
+
+    let TouchableCmp = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback
+    }
+
     return (
-        <TouchableOpacity 
-        style={styles.gridItem} 
-        onPress={props.onSelect}
-        >
-            <View style={{backgroundColor: props.color}}>
-                <Text>{props.title}</Text>
+        <View style={styles.gridItem}>
+        <TouchableCmp 
+        style={{flex: 1}}
+        onPress={props.onSelect}>
+            <View style={{...styles.container,...{backgroundColor: props.color}}}>
+                <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
             </View>
-        </TouchableOpacity>
+        </TouchableCmp>
+        </View>
     );
 };
 
@@ -18,8 +26,28 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: 150
+        height: 150,
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    container:{
+        flex: 1,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        elevation: 3,
+        padding: 15,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'right'
     }
+
 });
 
 export default CategoryGrid;
